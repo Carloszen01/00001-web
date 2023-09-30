08c935c38b50549d19b15df51974558a2236bfcd
elcharly669 <elcharly669@gmail.com>
2023-09-30 13:13:19 -0600

    30/09

--- src/components/Confiabilidad.jsx
+++ src/components/Confiabilidad.jsx
@@ -4,7 +4,8 @@ import styles, { layout } from "../style";
 import Button from "./Button";
 
 const FeatureCard = ({ icon, title, content, index }) => (
-    <div className={`py-0 flex flex-row rounded-[20px] lg:px-5 ${index !== features.length - 1 ? "mb-6" : "mb-0"} `}>
+    <div className={`py-0 flex flex-row rounded-[20px] lg:px-5
+    ${index !== features.length - 1 ? "mb-6" : "mb-0"} `}>
         <div className={`hidden w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
             <div className="flex flex-col items-center justify-center h-full">
                 <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
@@ -26,18 +27,18 @@ const FeatureCard = ({ icon, title, content, index }) => (
 
 const Confiabilidad = () => (
     <section id="producto" className={`${layout.section} sm:p-10 p-6 bg-black-gradient pb-10
-    my-2 min-w-[140px] rounded-xl sidebar`}>
+    my-2 min-w-[140px] rounded-xl sidebar items-start`}>
         <div className={`${layout.sectionInfo}`}>
             <h2 className='font-poppins font-semibold xs:text-[38px] text-[30px]
             text-white xs:leading-[56.8px] leading-[46.8px] w-full mb-5'>
                 Ingredientes seguros, <br className="sm:block hidden" /> puros y orgánicos.
             </h2>
-            <p className={`${styles.paragraph} max-w-[700px]`}>La Melena de León es un hongo medicinal con potentes beneficios cognitivos. Nuestras cápsulas están elaboradas con ingredientes seguros, puros y orgánicos para que puedas disfrutar de todos sus beneficios sin preocupaciones.</p>
-            <img src={lions} alt="Hongo Melena de León" className="dark:shadow-gray-800
-             h-auto max-w rounded-lg shadow mt-8 max-w-[600px] w-[100%]"/>
+            <p className={`${styles.paragraph} max-w-[900px]`}>La Melena de León es un hongo medicinal con potentes beneficios cognitivos. Nuestras cápsulas están elaboradas con ingredientes seguros, puros y orgánicos para que puedas disfrutar de todos sus beneficios sin preocupaciones.</p>
+            <img src={lions} alt="Hongo Melena de León" className={`dark:shadow-gray-800
+             h-auto max-w rounded-lg shadow mt-8 max-w-[600px] w-[100%]`} />
         </div>
 
-        <div className={`flex-1 flex max-w-[700px]
+        <div className={`flex-1 flex max-w-[900px]
         md:ml-10 ml-0 md:mt-0 mt-10 relative flex-col`}>
             {features.map((feature, index) => (
                 <FeatureCard key={feature.id} {...feature} index={index} />
--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,54 +1,94 @@
-import React, { useState } from "react";
-import styles, { layout } from "../style";
+import React, { useState, useCallback } from "react";
+import styles from "../style";
 import { faq } from "../constants";
+
+const FAQItem = ({ faq, isOpen, toggleQuestion }) => {
+    const { title, content } = faq;
+
+    return (
+        <div className="items-center flex-start flex-col mb-10">
+            <button
+                className="flex items-center focus:outline-none"
+                onClick={toggleQuestion}
+            >
+                <svg
+                    className={`flex-shrink-0 w-6 h-6 ${isOpen ? "text-green-300" : "text-green-300"
+                        }`}
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M20 12H4"
+                    ></path>
+                </svg>
+                <h4
+                    className={`mx-4 font-poppins text-[20px] leading-[32px] text-white ${isOpen ? "text-white" : "text-white dark:text-white"
+                        }`}
+                >
+                    {title}
+                </h4>
+            </button>
+            <div
+                className={`overflow-hidden transition-all max-h-0 ${isOpen ? "max-h-[900px]" : "max-h-0"
+                    }`}
+            >
+                <div className="flex mt-6 md:mx-10">
+                    <span
+                        className={`border ${isOpen ? "border-green-300" : "border-green-300"
+                            }`}
+                    ></span>
+                    <p className={`${styles.paragraph} px-4 break`}>{content}</p>
+                </div>
+            </div>
+            <hr className="h-px my-8 bg-gray-500 border-0" />
+        </div>
+    );
+};
 
 const FAQ = () => {
     const [openQuestion, setOpenQuestion] = useState(null);
 
-    const toggleQuestion = (index) => {
-        if (openQuestion === index) {
-            setOpenQuestion(null);
-        } else {
-            setOpenQuestion(index);
-        }
-    };
+    const toggleQuestion = useCallback(
+        (index) => {
+            setOpenQuestion((prevOpenQuestion) =>
+                prevOpenQuestion === index ? null : index
+            );
+        },
+        [setOpenQuestion]
+    );
 
     return (
         <section className={`${styles.paddingX} my-16`}>
-            <h2 className='font-poppins font-semibold xs:text-[38px] text-[30px]
-            text-white xs:leading-[56.8px] leading-[46.8px] w-full mb-5 text-center lg:mb-20'>
+            <h2 className="font-poppins font-semibold xs:text-[38px] text-[30px] text-white xs:leading-[56.8px] leading-[46.8px] w-full mb-5 text-center lg:mb-20">
                 Preguntas frecuentes
             </h2>
-            <div class={`flex-1 mt-8 lg:mx-12 lg:mt-0 pt-5`}>
+            <div className={`flex-1 mt-8 lg:mx-12 lg:mt-0 pt-5`}>
                 {faq.map((faq, index) => (
-                    <div className={`items-center flex-start flex-col mb-10`} key={index}>
-                        <button class="flex items-center focus:outline-none" onClick={() => toggleQuestion(index)}>
-                            <svg class={`flex-shrink-0 w-6 h-6 ${openQuestion === index ? "text-green-300" : "text-green-300"}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4"></path></svg>
-                            <h4 class={`mx-4 font-poppins text-[20px] leading-[32px] text-white ${openQuestion === index ? "text-white" : "text-white dark:text-white"}`}>{faq.title}</h4>
-                        </button>
-                        <div class={`overflow-hidden transition-all max-h-0 
-                        ${openQuestion === index ? "max-h-[900px]" : "max-h-0"}`}>
-                            <div class="flex mt-6 md:mx-10">
-                                <span class={`border ${openQuestion === index ? "border-green-300" : "border-green-300"}`}></span>
-                                <p class={`${styles.paragraph} max-w-4xl px-4 break`}>
-                                    {faq.content}
-                                </p>
-                            </div>
-                        </div>
-                        <hr class="h-px my-8 bg-gray-500 border-0" />
-                    </div>
+                    <FAQItem
+                        key={index}
+                        faq={faq}
+                        isOpen={openQuestion === index}
+                        toggleQuestion={() => toggleQuestion(index)}
+                    />
                 ))}
             </div>
             <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
                 <a href="https://api.whatsapp.com/send?phone=%2B523325069894&text=%C2%A1Hola!+Tengo+algunas+dudas+sobre+Utropix.">
-                    <button type="button" className={`mt-6 py-4 px-6 font-poppins font-medium
-                    text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+                    <button
+                        type="button"
+                        className={`mt-6 py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+                    >
                         Pregúntanos
                     </button>
                 </a>
             </div>
         </section>
     );
-}
+};
 
 export default FAQ;
