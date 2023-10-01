bd203e8c0a7f0edd78be7024824f3c64ec59693b
elcharly669 <elcharly669@gmail.com>
2023-09-30 20:34:29 -0600

    last

--- src/components/Promo.jsx
+++ src/components/Promo.jsx
@@ -3,9 +3,11 @@ import { test, test2 } from '../assets'
 
 const Promo = () => {
     return (
-        <div className='w-full ss:px-10 mb-10'>
-            <img className='ss:flex hidden' src={test} alt="promoción $299" />
-            <img className='ss:hidden flex' src={test2} alt="romoción $299" />
+        <div className='w-full ss:px-0 mb-10 flex flex-col justify-center'>
+            <div className='flex justify-center'>
+                <img className='ss:flex hidden justify-center items-center max-w-[1100px]' src={test} alt="promoción $299" />
+                <img className='ss:hidden flex flex justify-center items-center' src={test2} alt="romoción $299" />
+            </div>
             <div className="flex justify-center relative botton-0 lg:left-0 left-0 lg:right-0 right-0 text-center">
                 <div className={`${styles.flexCenter} mt-6 w-[200px] h-[50px] rounded-[10px] bg-blue-gradient p-[1px] cursor-pointer`}>
                     <div className={`${styles.flexCenter} items-center bg-primary w-[100%] h-[100%] rounded-[10px]`}>
