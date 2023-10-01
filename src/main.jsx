cf96e4a3045dd51f7deb6193fca19a642dca2e3d
elcharly669 <elcharly669@gmail.com>
2023-10-01 17:09:11 -0600

    last

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import Home from './Home';
 
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createHashRouter, RouterProvider, Navigate } from 'react-router-dom';
 import './index.css';
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/*',
+    element: <Navigate to="/" />
+  },
+  {
+    path: '/compra',
+    element: <Navigate to="/" />
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
