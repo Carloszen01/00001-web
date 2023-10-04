282ae17736d641ad08f18519fa31169737eeaf4f
elcharly669 <elcharly669@gmail.com>
2023-10-04 11:46:24 -0600

    pixel

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { Routex } from "./Routes"
+import ReactPixel from 'react-facebook-pixel'
+
+const pixel = new ReactPixel({
+  pixelId: "858411422659360",
+  options: {
+    autoConfig: true,
+  },
+});
 
 const App = () => {
   return (
-    <Routex />
+    <ReactPixel pixel={858411422659360}>
+      <Routex />
+      ReactPixel.pageView();
+    </ReactPixel>
   )
 }
 
