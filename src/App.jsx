3caf8eee592639ef6b59ad6a65a60348dd07e404
elcharly669 <elcharly669@gmail.com>
2023-10-08 10:59:54 -0600

    metatags

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Routex } from "./Routes"
 
+
 const App = () => {
   return (
     <Routex />
-  )
+  );
 }
 
 export default App
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,6 @@
 import styles from './style';
 import ReactPixel from 'react-facebook-pixel'
-import { Helmet } from 'react-helmet-async';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import { Beneficios, Confiabilidad, FAQ, Testimonios, CTA, Footer, Navbar, Hero, Stats, Consumo, Promo } from './components/inddex';
 
@@ -8,34 +8,45 @@ ReactPixel.init('858411422659360', {}, { debug: false, autoConfig: true });
 ReactPixel.pageView();
 
 const Home = () => (
-  //<title>Inicio</title>
-  //<meta name="description" content="Mejora tu rendimiento cognitivo con nootrópicos naturales" />
-  //<link rel="canonical" href='/' />
-  <div className='bg-primary w-full overflow-hidden'>
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
+  <HelmetProvider>
+    <Helmet prioritizeSeoTags>
+      <title>Utropix: Mejor Memoria y Concentración con Nootrópicos</title>
+      <meta name='description' content="Potencia tu mente con nootrópicos naturales. Prueba los beneficios del hongo Melena de León." />
+      <link rel="canonical" href="https://utropix.net" />
+      <meta name='keywords' content='hongo melena de león, nootrópicos, nootrópicos naturales, 
+      suplementos para la mente, aumentar rendimiento cognitivo' />
+      { /* Facebook tags */}
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Utropix: Mejor Memoria y Concentración con Nootrópicos" />
+      <meta property="og:description" content="Potencia tu mente con nootrópicos naturales. Prueba los beneficios del hongo Melena de León." />
+      { /* End Facebook tags */}
+    </Helmet>
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
-    </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Beneficios />
+          <Confiabilidad />
+          <Consumo />
+          <FAQ />
+          <Promo />
+          <CTA />
+          <Footer />
+        </div>
       </div>
     </div>
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Stats />
-        <Beneficios />
-        <Confiabilidad />
-        <Consumo />
-        <FAQ />
-        <Promo />
-        <CTA />
-        <Footer />
-      </div>
-    </div>
-  </div>
+  </HelmetProvider>
 )
 
 export default Home;
